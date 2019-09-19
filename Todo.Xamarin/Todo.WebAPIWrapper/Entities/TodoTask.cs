2ebe5a4ebc73de01d152f851f615e4c232df66b6
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-09-19 18:54:13 +0200

    Create WebAPIWrapper csproj

---
+++ Todo.Xamarin/Todo.WebAPIWrapper/Entities/TodoTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Todo.WebAPIWrapper.Entities
+{
+    public class TodoTask
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
